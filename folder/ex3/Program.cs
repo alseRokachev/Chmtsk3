ea845912190addacc46bc293378fccbca62b9183
Alex Rokachev <alserokachev@gmail.com>
2023-01-21 20:03:55 +0300

    made ex3

---
+++ folder/ex3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+System.Console.Write("Введите число: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+if (numb > 1)
+{
+    for (int i = 1; i <= numb; i++)
+    {
+        double answer = Math.Pow(i, 3);
+        System.Console.WriteLine(answer);
+    }
+}
+else
+{
+    System.Console.WriteLine("Некорректное число");
+}
